faf4edc659df2cafb4b2a4c1222ca523d449dc36
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2025-05-21 12:05:10 +0100

    fix(report-card): update image source to use imageAddress and remove unused getPreviewUrl function

--- config/main.ts
+++ config/main.ts
@@ -1,6 +1,5 @@
 const LOCAL_ADDRESS = "http://localhost:8000/api";
-const PRODUCTION_ADDRESS =
-  "https://walrus-app-65k3h.ondigitalocean.app/api";
+const PRODUCTION_ADDRESS = "https://walrus-app-65k3h.ondigitalocean.app/api";
 export const devEnv = "development" === process.env.NODE_ENV;
 export const serverAddress = devEnv ? LOCAL_ADDRESS : PRODUCTION_ADDRESS;
 export const LOCAL_IMAGE = "http://localhost:8000/storage/";
@@ -8,5 +7,5 @@ export const NEXT_PUBLIC_IMAGE_PROTOCOL = devEnv ? "http" : "https";
 export const NEXT_PUBLIC_IMAGE_HOSTNAME = devEnv
   ? "localhost"
   : "pfemanager.lon1.digitaloceanspaces.com";
-export const PRODUCTION_IMAGE = `${NEXT_PUBLIC_IMAGE_PROTOCOL}://${NEXT_PUBLIC_IMAGE_HOSTNAME}/storage/`;
+export const PRODUCTION_IMAGE = `${NEXT_PUBLIC_IMAGE_PROTOCOL}://${NEXT_PUBLIC_IMAGE_HOSTNAME}/`;
 export const imageAddress = devEnv ? LOCAL_IMAGE : PRODUCTION_IMAGE;
